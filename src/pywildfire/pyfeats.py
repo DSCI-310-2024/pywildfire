1fa67f157a9c687a7a4f37cbcc5e3c118ce87712
Lilmillyrock <lilliantm7@gmail.com>
2024-04-11 13:00:52 -0700

    docs: added docstrings and example usage to relevant_features and calculate_rmse

--- src/pywildfire/pyfeats.py
+++ src/pywildfire/pyfeats.py
@@ -2,25 +2,36 @@ import pandas as pd
 import numpy as np
 
 def relevant_features(correlation_matrix, target_variable):
-    ### STILL MISSING DOCSTRING
     """
-    ...
+    Takes a correlation matrix and a target variable and returns a list of relevant features.
 
     Parameters
     ----------
-    correlation_matrix: ...
-        ...
-    output_path: ...
-        ...
+    correlation_matrix: pandas.DataFrame
+        Correlation matrix where rows and columns represent features, and values represent coefficients.
+    target_variable: str
+        Target variable for which relevant features will be identified.
 
     Returns
     -------
     pandas.Series
-        ...
+        List of relevant features, in descending order based on coefficient.
         
     Examples
     --------
-    >>> ...
+    >>> from pywildfire.pyfeats import relevant_features
+    >>> corr_matrix = pd.DataFrame({
+    ...     'A': {'A': 1.0, 'B': -0.8, 'C': 1.0, 'D': -0.8},
+    ...     'B': {'A': -0.8, 'B': 1.0, 'C': -0.8, 'D': 1.0},
+    ...     'C': {'A': 1.0, 'B': -0.8, 'C': 1.0, 'D': -0.8},
+    ...     'D': {'A': -0.8, 'B': 1.0, 'C': -0.8, 'D': 1.0}
+    ... })
+    >>> target_var = 'A'
+    >>> relevant_features(corr_matrix, target_var)
+    C    1.0
+    B   -0.8
+    D   -0.8
+    Name: A, dtype: float64
     """
     # Check for empty inputs
     if target_variable not in correlation_matrix.columns:
--- src/pywildfire/pywildfire.py
+++ src/pywildfire/pywildfire.py
@@ -2,30 +2,47 @@ import pandas as pd
 import numpy as np
 
 def calculate_rmse(observed, predicted):
-    ### STILL MISSING DOCSTRING
     """
-    ...
+    Calculate the Root Mean Squared Error (RMSE) between observed and predicted values.
 
     Parameters
     ----------
-    observed: ...
-        ...
-    predicted: ...
-        ...
+    observed: array-like
+        List, tuple, numpy array, or pandas.Series containing the observed values.
+    predicted: array-like
+        List, tuple, numpy array, or pandas.Series of the same length as `observed`, 
+        containing the predicted values.
 
     Returns
     -------
     float
-        ...
+        The RMSE between observed and predicted values.
 
     Raises
     ------
     ValueError
-        ...
+        If `observed` or predicted` is empty.
+        If `observed` and predicted` have different lengths.
+        If `observed` or predicted` is not of type list, tuple, numpy array, or pandas.Series.
     
     Examples
     --------
-    >>> ...
+    >>> from pywildfire.pywildfire import calculate_rmse
+    
+    >>> observed = pd.Series([4, 8, 5, 3, 7])
+    >>> predicted = pd.Series([16, 4, 7, 9, 3])
+    calculate_rmse(observed, predicted)
+    6.572670690061994
+    
+    >>> observed = [4, 8, 5, 3, 7]
+    >>> predicted = [16, 4, 7, 9, 3]
+    calculate_rmse(observed, predicted)
+    6.572670690061994
+
+    >>> observed = np.array([4, 8, 5, 3, 7])
+    >>> predicted = np.array([16, 4, 7, 9, 3])
+    calculate_rmse(observed, predicted)
+    6.572670690061994
     """
     # Check for empty inputs
     if isinstance(observed, (list, tuple, np.ndarray, pd.Series)) and isinstance(predicted, (list, tuple, np.ndarray, pd.Series)):
